b97247f053987f062143027e42a7392f33c0381b
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-27 13:37:11 -0300

    Consulta via JSON.

--- src/main/java/com/swa/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/swa/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.swa.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.swa.workshopmongo.domain.Post;
@@ -10,6 +11,8 @@ import com.swa.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{ 'title': { $regex:  ?0, $options: 'i' } }")
+	List<Post> findByTitleJSON(String str);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }

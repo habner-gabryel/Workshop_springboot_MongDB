1e4a6ba1ad4dc5a417356513f948b52187da5d22
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-27 11:21:16 -0300

    Projeção de dados do autor com DTO.

--- src/main/java/com/swa/workshopmongo/config/Instantiation.java
+++ src/main/java/com/swa/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.swa.workshopmongo.domain.Post;
 import com.swa.workshopmongo.domain.User;
+import com.swa.workshopmongo.dto.AuthorDTO;
 import com.swa.workshopmongo.repositories.PostRepository;
 import com.swa.workshopmongo.repositories.UserRepository;
 
@@ -27,18 +28,20 @@ public class Instantiation implements CommandLineRunner {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		userRepository.deleteAll();
 		
 		User u1 = new User(null, "Frantiesco Virgulini", "fiuuummm@gmail.com"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com"); 
 		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post p1 = new Post(null, sdf.parse("21/03/2018") ,"Ban Dia", "uso òculos", u1);
-		Post p2 = new Post(null, sdf.parse("14/06/2020") ,"Olá marilene", "vinho, tainha e muita noite", u1);
-		
-		userRepository.deleteAll();
-		postRepository.deleteAll();
 
 		userRepository.saveAll(Arrays.asList(u1,u2,u3));
+		postRepository.deleteAll();
+		
+		Post p1 = new Post(null, sdf.parse("21/03/2018") ,"Ban Dia", "uso òculos", new AuthorDTO(u1));
+		Post p2 = new Post(null, sdf.parse("14/06/2020") ,"Olá marilene", "vinho, tainha e muita noite", new AuthorDTO(u1));
+		
+
 		postRepository.saveAll(Arrays.asList(p1, p2));
 	}
 

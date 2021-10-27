c5dfe7ead0f2735330a3ad18b5927b062d84b5d1
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-27 10:09:53 -0300

    Retornando user por id

--- src/main/java/com/swa/workshopmongo/services/UserService.java
+++ src/main/java/com/swa/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.swa.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.swa.workshopmongo.domain.User;
 import com.swa.workshopmongo.repositories.UserRepository;
+import com.swa.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto com o id " + id + " não existe."));
 	}
 }

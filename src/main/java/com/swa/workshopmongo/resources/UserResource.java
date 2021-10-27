25092086213c173c08147d4fb30176d25827b240
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-27 11:32:38 -0300

    Retorno de posts do usuário.

--- src/main/java/com/swa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/swa/workshopmongo/resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.swa.workshopmongo.domain.Post;
 import com.swa.workshopmongo.domain.User;
 import com.swa.workshopmongo.dto.UserDTO;
 import com.swa.workshopmongo.services.UserService;
@@ -67,5 +68,12 @@ public class UserResource {
 		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts",method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }

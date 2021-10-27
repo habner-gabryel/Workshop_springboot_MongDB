3c9227279067c8a86c0c2fdcb434327a6b178d10
habner-gabryel <habner.gabryel.cc@gmail.com>
2021-10-27 10:29:12 -0300

    Adicionado Delete por id de user.

--- src/main/java/com/swa/workshopmongo/services/UserService.java
+++ src/main/java/com/swa/workshopmongo/services/UserService.java
@@ -30,8 +30,13 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
 	
 	public User fromDTO(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
+	
 }
